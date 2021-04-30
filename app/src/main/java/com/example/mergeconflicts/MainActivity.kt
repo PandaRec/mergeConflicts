6ded4f58d15913377776141bbe82fffb2a84d21e
Dmitry Pevnitsky <pandarec13@yandex.ru>
2021-04-30 18:31:21 +0300

    second commit in sec branch

--- app/src/main/java/com/example/mergeconflicts/MainActivity.kt
+++ app/src/main/java/com/example/mergeconflicts/MainActivity.kt
@@ -8,7 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val b=1
-        val c = 1
+
     }
 }
